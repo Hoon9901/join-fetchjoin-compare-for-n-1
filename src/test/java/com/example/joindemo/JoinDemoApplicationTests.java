b231de585ef296bc5a10f2869a66275f757d1ebe
Hoon9901 <shonn.dev@gmail.com>
2024-04-29 15:09:33 +0900

    init projects

---
+++ src/test/java/com/example/joindemo/JoinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
